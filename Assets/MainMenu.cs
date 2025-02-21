265c5e30ef6b447230386aeb73e8f20e48d57ede
DaJoka <114927429+Chertila112@users.noreply.github.com>
2025-02-21 19:53:16 +0300

    MainMenu first iter

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // Метод для запуска игры
+    public GameObject settings;
+    public void PlayGame()
+    {
+        // Загрузка следующей сцены по порядку в Build Settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Метод для перехода в настройки
+    public void OpenOptions()
+    {
+        settings.SetActive(true);
+    }
+
+    // Метод для выхода из игры
+    public void QuitGame()
+    {
+        Debug.Log("Игра закрыта!");
+        Application.Quit();
+    }
+
+    public void OnBack()
+    {
+        settings.SetActive(false);
+    }
+}
