43cc1f7a294991f8880ecf0e1e5734001b0d15ae
WarLockeR31 <roman.borisenko2016@gmail.com>
2025-02-21 17:25:43 +0300

    Feat: DealerEnemy

---
+++ Assets/CardProjectile.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardProjectile : MonoBehaviour
+{
+    [SerializeField] private float autoAimSeconds;
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private float rotationSpeed;
+    private bool isAutoAiming = true;
+    private Vector3 direction;
+    private Player player;
+    private float damage; public void SetDamage(float newValue) { damage = newValue; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Player.Instance;
+        StartCoroutine(DisableAutoAim());
+    }
+
+    IEnumerator DisableAutoAim()
+    {
+        yield return new WaitForSeconds(autoAimSeconds);
+        isAutoAiming = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAutoAiming)
+        {
+            Vector3 playerPos = player.transform.position;
+            direction = (playerPos - transform.position).normalized;
+        }
+
+        transform.position += direction * projectileSpeed * Time.deltaTime;
+
+        transform.eulerAngles += new Vector3(0f, rotationSpeed * Time.deltaTime, 0f);
+        //transform.Rotate();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.TakeDamage(damage);
+            Destroy(gameObject);
+            return;
+        }
+
+        if (other.CompareTag("Enemy") || other.CompareTag("EnemyProjectile") || other.isTrigger)
+        {
+            return;
+        }
+
+        Destroy(gameObject);
+    }
+}
